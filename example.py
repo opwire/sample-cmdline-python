aa4025a9c9e9dad1db7ee0c3e2b6f74d0357accb
pnhung177 <pnhung177@acegik.net>
2019-03-12 10:47:18 +0700

    Parsing input to JSON object

--- example.py
+++ example.py
@@ -4,11 +4,19 @@ import sys
 def bootstrap():
   _store = {}
 
+  _store["input"] = load_input()
+
+  return _store
+
+def load_input():
   _input = ''
   for line in sys.stdin:
     _input += line
-  _store["input"] = _input
-
-  return _store
+  try:
+    _inputJSON = json.loads(_input)
+    return _inputJSON
+  except:
+    return _input
+  pass
 
 sys.stdout.write(json.dumps(bootstrap(), indent=2))
